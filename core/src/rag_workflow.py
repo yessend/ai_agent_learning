d9d5431593a316311a638f23384b7c3a8405afc7
Yessen <esen0105@mail.ru>
2025-12-14 20:03:15 +0500

    Added short-term memory to the agent

---
+++ core/src/rag_workflow.py
@@ -0,0 +1,7 @@
+# Implement a custom RAG System from scratch as a class 
+# allowing multiple users have a chat history.
+
+class RagSystem:
+    """
+    Rag pipeline implemented as a workflow.
+    """
