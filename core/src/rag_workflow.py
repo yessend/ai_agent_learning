095d15abeef70b4535a2b59145ac6d0ad4676144
Yessen <esen0105@mail.ru>
2025-12-16 16:24:59 +0500

    Removed unnecessary files

--- core/src/rag_workflow.py
+++ core/src/rag_workflow.py
@@ -1,4 +1,4 @@
-# Implement a custom RAG System from scratch as a class 
+# Implement a custom RAG System from scratch as subclass of Workflow 
 # allowing multiple users have a chat history.
 from llama_index.llms.google_genai import GoogleGenAI
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
@@ -8,51 +8,65 @@ from llama_index.core.tools import RetrieverTool
 from llama_index.core.retrievers import RouterRetriever
 from llama_index.core.selectors import LLMMultiSelector
 
-from llama_index.core.chat_engine.simple import SimpleChatEngine
-from llama_index.core.memory import Memory
-
 from core.config.config import Config
 from core.config.constants import RagConstants
+from core.helpers.chat_engine_registry import ChatEngineRegistry
 from core.helpers.logger import logger
 from core.helpers.json_extractor import extract_json_array
 
-class RagSystem:
-    """
-    Rag pipeline implemented as a workflow.
-    For now, let's just make it a class before transitioning it into the workflow.
-    """
+from llama_index.core.workflow import (
+    Workflow,
+    Context,
+    Event,
+    StartEvent,
+    StopEvent,
+    step
+)
+
+
+class RetrieverToolsEvent(Event):
+    retriever_tools: list[RetrieverTool]
+
+
+class RetrievalRelevantEvent(Event):
+    context: str | bool
+
+
+class RagWorkflow(Workflow):
+    # This is the whole RAG system implemented as a Workflow
     
     def __init__(
-        self,
+        self, 
+        chat_engine_registry: ChatEngineRegistry,
         router_llm: GoogleGenAI | None,
         chat_llm: GoogleGenAI | None,
-        embed_model: HuggingFaceEmbedding | None,
-        docs_path: str,
-        collections: dict   
+        embed_model: HuggingFaceEmbedding | None
     ):
+        super().__init__()
+        self.chat_engine_registry = chat_engine_registry
         self.router_llm = router_llm
         self.chat_llm = chat_llm
         self.embed_model = embed_model
-        self.docs_path = docs_path
-        self.collections = collections
-        
-        # We initialize it as an attribute since we won't need to recreate it many times
-        self.router_retriever = self._get_router_retriever()
-        
-        # Cached chat engines per user   
-        self.chat_engines_cached = {}
     
     
-    def _ingest(self) -> list[RetrieverTool]:
+    @step
+    async def _ingest(self, ctx: Context, ev: StartEvent) -> RetrieverToolsEvent | None:
         # Initialize the retriever_tools list to create a list of RetrieverTool objects that we will later
         # pass into the LLMMultiSelector for selecting an appropriate retriever
+        docs_path = ev.get("docs_path")
+        collections = ev.get("collections")
+        
+        if not docs_path or not collections:
+            logger.warning("Ingestion method cannot be performed, missing arguments in the Start Event.")
+            return None
+        
         retriever_tools = []
 
         # I manually wrote a dictionary for each course and its decription inside the previously loaded JSON file. 
         # 'collection_name' matches the name of the course folder inside the documents folder
-        for collection_name, collection_description in self.collections.items():
+        for collection_name, collection_description in collections.items():
 
-            collection_path = self.docs_path + '/' + collection_name
+            collection_path = docs_path + '/' + collection_name
 
             # 1) Read documents and create list of 'Document' objects, that has id_, metadata, text attributes.
             #    Document class (generic container for any data source) is a subclass of the TextNode class
@@ -82,10 +96,11 @@ class RagSystem:
             # 5) Append created RetrieverTool for each collection to the list initialized before this loop
             retriever_tools.append(collection_retriever_tool)
         
-        return retriever_tools
+        return RetrieverToolsEvent(retriever_tools = retriever_tools)
     
     
-    def _get_router_retriever(self) -> RouterRetriever:
+    @step
+    async def _get_router_retriever(self, ctx: Context, ev: RetrieverToolsEvent) -> StopEvent | None:
         # Create a router from that list of RetrieverTool objects using an LLMMultiSelector for selecting relevant retrievers 
         # based on a prompt
         router = RouterRetriever(
@@ -96,20 +111,37 @@ class RagSystem:
                 llm = self.router_llm
             ),
             llm = self.router_llm,
-            retriever_tools = self._ingest()
+            retriever_tools = ev.retriever_tools
         )
-        return router
+        return StopEvent(result = router)
     
     
-    async def _is_retrieval_relevant(self, user_query: str) -> str | bool:
+    @step
+    async def _is_retrieval_relevant(self, ctx: Context, ev: StartEvent) -> RetrievalRelevantEvent | None:
         # Check the relevance of the nodes retrieved from router retriever.
         # We will return either False value if no retrieval is irrelevant or the retrieved
         # context otherwise
+        router_retriever = ev.get("router_retriever")
+        user_query = ev.get("user_query")
+        user_name = ev.get("user_name")
+        user_id = ev.get("user_id")
+        
+        if not user_query or not router_retriever or not user_name or not user_id:
+            logger.warning("Relevancy check cannot be performed, missing arguments in the Start Event.")
+            return None
+        
+        await ctx.store.set("user_query", user_query)
+        if user_name:
+            await ctx.store.set("user_name", user_name)
+        if user_id:
+            await ctx.store.set("user_id", user_id)
+        
+        
         try:
-            retrieved_nodes = await self.router_retriever.aretrieve(user_query)
+            retrieved_nodes = await router_retriever.aretrieve(user_query)
         except ValueError as e:
             logger.warning(f"{e}: knowledge base does not contain relevant info; no nodes were retrieved")
-            return False
+            return RetrievalRelevantEvent(context = False)
         
         # Create retrieved_nodes str for LLM to easier make a choice
         retrieved_nodes_str = ""
@@ -135,7 +167,8 @@ class RagSystem:
         relevant_node_ids = extract_json_array(response_relevance.text.strip())
         
         if not relevant_node_ids:
-            return False
+            logger.warning("Among retrieved nodes, no nodes contain relevant information to the user's query.")
+            return RetrievalRelevantEvent(context = False)
         
         # if not relevant_node_ids:
         #     logger.warning("Retrieved nodes do not containt a relevant info.")
@@ -145,48 +178,31 @@ class RagSystem:
         context = "\n\n".join(
             [retrieved_nodes_dict[node_id] for node_id in relevant_node_ids]
         )
-        return context
+
+        return RetrievalRelevantEvent(context = context)
     
     
-    def _get_or_create_chat_engine(self, user_id: str) -> SimpleChatEngine:
-        # Here we either get cached chat_engine (SimpleChatEngine object) for a specific user 
-        # or we create a new chat_engine for the user if he hadn't one before.
-        if user_id in self.chat_engines_cached:
-            logger.info(f"Using cached engine for the user {user_id}")
-            return self.chat_engines_cached[user_id]
-        
-        logger.info(f"Creating a new chat engine for user {user_id}")
-        
-        memory = Memory.from_defaults(
-            session_id = "Test_session_id", # change it later on a correct session_id
-            token_limit = 2000,
-            chat_history_token_ratio = 1.0
-        )
-        
-        chat_engine = SimpleChatEngine.from_defaults(
-            llm = self.chat_llm,
-            memory = memory
-        )
-        self.chat_engines_cached[user_id] = chat_engine
-        return chat_engine
-    
-    
-    async def query(self, user_name: str, user_id: str, user_query: str) -> str:
+    @step
+    async def _synthesize(self, ctx: Context, ev: RetrievalRelevantEvent) -> StopEvent | None:
         # Here we produce the final response to the user's query
+        user_query = await ctx.store.get("user_query", default = None)
+        user_name = await ctx.store.get("user_name", default = None)
+        user_id = await ctx.store.get("user_id", default = None)
         
-        context = await self._is_retrieval_relevant(user_query)
+        context = ev.context
         prompt = ""
         
         if not context:
+            logger.warning("No context is provided.")
             prompt = f"Name of the user: {user_name}; question: {user_query}"
         else:
-            prompt = RagConstants.SYSTEM_PROMPT.format(
-                user_name = user_name,
-                question = user_query,
-                context = context
-            )
+            prompt = RagConstants.SYSTEM_PROMPT_WORKFLOW + f"""
+                    User's name: {user_name}
+                    User's question: {user_query}
+                    Context: {context}
+                    """
         
-        chat_engine = self._get_or_create_chat_engine(user_id)
+        chat_engine = self.chat_engine_registry.get_or_create_chat_engine(user_id)
         
         response = await chat_engine.achat(prompt)
-        return response.response
+        return StopEvent(result = response.response)
