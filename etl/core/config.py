492e8f8eb5459872871a8e2d17e0da20026dcec7
Yessen <esen0105@mail.ru>
2026-02-07 11:18:24 +0500

    Updated constants

--- etl/core/config.py
+++ etl/core/config.py
@@ -4,10 +4,10 @@ import os
 class ETLConfig:
     
     DATA_DIR = Path("./data")
-    RAW_DIR = DATA_DIR / "01_raw"
-    PARSED_DIR = DATA_DIR / "02_parsed"
-    CLEANED_DIR = DATA_DIR / "03_cleaned"
-    CHUNKED_DIR = DATA_DIR / "04_chunked"
+    RAW_DIR = DATA_DIR / "pdfs"
+    PARSED_DIR = DATA_DIR / "parsed"
+    CLEANED_DIR = DATA_DIR / "cleaned"
+    CHUNKED_DIR = DATA_DIR / "chunked"
     
     QDRANT_URL = os.getenv("QDRANT_URL", "http://localhost:6333")
     EMBEDDING_MODEL = "intfloat/multilingual-e5-large-instruct"
